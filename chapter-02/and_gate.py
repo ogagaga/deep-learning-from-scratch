c0ef2eca2f7f4193215d0114f0691ef442c62952
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2017-01-20 17:36:01 +0900

    Add bias for AND function

--- chapter-02/and_gate.py
+++ chapter-02/and_gate.py
@@ -1,12 +1,16 @@
+import numpy as np
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp =  x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array([0, 1])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    sum_value = np.sum(w*x) + b
+    if sum_value <= 0:
         return 0
-    elif tmp > theta:
+    else:
         return 1
 
-AND(0, 0)
-AND(1, 0)
-AND(0, 1)
-AND(1, 1)
+# AND(0, 0)
+# AND(1, 0)
+# AND(0, 1)
+# AND(1, 1)
