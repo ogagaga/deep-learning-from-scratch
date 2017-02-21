07f2a4764a50345f5d721c88125e7f871196fad7
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2017-02-21 10:13:03 +0900

    Add sigmoid function

---
+++ chapter-03/sigmoid.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+## シグモイド関数
+#  h(x) = 1 / 1 + exp(-x)
+#
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = sigmoid(X)
+plt.plot(X, Y)
+plt.plot(X, Y)
+plt.ylim(-0.1, 1.1)
+plt.show()
